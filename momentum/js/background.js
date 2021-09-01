6c72dff78da7b39e02446b382483d70386753470
eunsuneun <eunsunbsn@gmail.com>
2021-09-01 18:26:04 +0900

    Update Random BG in Momentum

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const images = ['01.png', '02.png', '03.png', '04.png', '05.png'];
   const randomIdx = Math.floor(Math.random() * images.length);
   const chosenImg = images[randomIdx];
-  const $image = document.createElement('img');
-  $image.src = `img/${chosenImg}`;
-  document.body.appendChild($image);
+
+  document.body.style.background = `url(img/${chosenImg}) no-repeat center / cover`;
 });
