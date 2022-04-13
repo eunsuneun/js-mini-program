2d5d3cd095eaa2bf6aa5a35176a80983c01b0bc6
eunsuneun <eunsunbsn@gmail.com>
2022-04-13 18:08:29 +0900

    delete momentum, to-do-list

--- momentum/js/background.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const images = ['01.png', '02.png', '03.png', '04.png', '05.png'];
-  const randomIdx = Math.floor(Math.random() * images.length);
-  const chosenImg = images[randomIdx];
-
-  document.body.style.background = `url(img/${chosenImg}) no-repeat center / cover`;
-});
